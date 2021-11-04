967de03c48c4822cb66fe7c0bdd1f4692432c6de
zhujianwei_glb <jianwei_zhu959@163.com>
2021-11-04 20:58:01 +0800

    master

---
+++ testscanf.c
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ZhuJianwei
+ * @Date: 2021-11-04 20:54:14
+ * @LastEditors: ZhuJianwei
+ * @LastEditTime: 2021-11-04 20:56:25
+ */
+#include"stdio.h"
+
+int main(int argc,char* argv[]){
+    int num = 0;
+    printf("请输入一个整形数字");
+    scanf("%d",&num);
+    return 0;
+}
