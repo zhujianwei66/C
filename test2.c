dce326619e97449b15dc071d49b26420eb73633a
zhujianwei_glb <jianwei_zhu959@163.com>
2021-11-05 12:10:30 +0800

    master

---
+++ test2.c
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ZhuJianwei
+ * @Date: 2021-11-05 12:04:36
+ * @LastEditors: ZhuJianwei
+ * @LastEditTime: 2021-11-05 12:09:13
+ */
+#include"stdio.h"
+int a = 10;
---
+++ testtest.c
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ZhuJianwei
+ * @Date: 2021-11-05 12:06:20
+ * @LastEditors: ZhuJianwei
+ * @LastEditTime: 2021-11-05 12:08:47
+ */
+#include "stdio.h"
+#include "test2.c"
+extern int a;
+
+int main(int argc, char* argv[]){
+    printf("%d",a);
+    return 0;
+}
