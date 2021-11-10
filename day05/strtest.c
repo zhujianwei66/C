7e2c1ef822368c81aef8dbf7d9fe62b7d7dbd753
zhujianwei_glb <jianwei_zhu959@163.com>
2021-11-10 10:07:27 +0800

    master

---
+++ day05/strtest.c
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ZhuJianwei
+ * @Date: 2021-11-09 17:33:36
+ * @LastEditors: ZhuJianwei
+ * @LastEditTime: 2021-11-09 17:43:19
+ */
+#include"stdio.h"
+#include"string.h"
+
+int main(int argc, char* argv[])
+{
+    char arr[] = "\a";
+    printf("sizeof(arr) =  %d \n",sizeof(arr));
+    printf("strlen(arr) = %d \n",strlen(arr));
+    return 0;
+}
